e28387427f9df523569172a005c691d113b4ff1c
azizganiyu <azizganiyu0@gmail.com>
2025-01-16 23:15:20 +0100

    update

--- src/database/seeders/seeder.service.ts
+++ src/database/seeders/seeder.service.ts
@@ -11,7 +11,7 @@ import { ConfigSeed } from './seeds/config.seed';
 
 @Injectable()
 export class SeederService {
-  private seeds = [ProductCategorySeed];
+  private seeds = [RoleSeed, AdminSeed, ProductSeed];
   // private seeds = [
   //   RoleSeed,
   //   AdminSeed,
