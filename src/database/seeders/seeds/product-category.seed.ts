205dcae024669b2ccf63c52e382dd580f7467748
azizganiyu <azizganiyu0@gmail.com>
2025-01-16 23:12:41 +0100

    update

--- src/database/seeders/seeds/product-category.seed.ts
+++ src/database/seeders/seeds/product-category.seed.ts
@@ -5,32 +5,32 @@ const values: ProductCategory[] = [
   {
     id: 'groceries',
     name: 'Groceries',
-    iconUrl: 'http://localhost:3000/uploads/system/groceries.svg',
+    iconUrl: 'https://api.shopnownow.app/uploads/system/groceries.svg',
   },
   {
     id: 'home-body-care',
     name: 'Home & Body Care',
-    iconUrl: 'http://localhost:3000/uploads/system/home-body-care.svg',
+    iconUrl: 'https://api.shopnownow.app/uploads/system/home-body-care.svg',
   },
   {
     id: 'alcoholic-drink',
     name: 'Alcoholic Drinks',
-    iconUrl: 'http://localhost:3000/uploads/system/alcoholic-drink.svg',
+    iconUrl: 'https://api.shopnownow.app/uploads/system/alcoholic-drink.svg',
   },
   {
     id: 'gift-hampers-cakes',
     name: 'Gift Hampers & Cakes',
-    iconUrl: 'http://localhost:3000/uploads/system/gift-hampers-cakes.svg',
+    iconUrl: 'https://api.shopnownow.app/uploads/system/gift-hampers-cakes.svg',
   },
   {
     id: 'chopnownow-food',
     name: 'Chopnownow Food',
-    iconUrl: 'http://localhost:3000/uploads/system/chopnownow-food.svg',
+    iconUrl: 'https://api.shopnownow.app/uploads/system/chopnownow-food.svg',
   },
   {
     id: 'meat-fish-poultry',
     name: 'Meat, Fish & Poultry',
-    iconUrl: 'http://localhost:3000/uploads/system/meat-fish-poultry.svg',
+    iconUrl: 'https://api.shopnownow.app/uploads/system/meat-fish-poultry.svg',
   },
 ];
 
